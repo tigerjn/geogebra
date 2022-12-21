df3a07f0639e48a5e61146b7bef9da0c0a9731fa
Laszlo Gal <laszlo@geogebra.org>
2022-12-21 16:02:07 +0100

    APPS-4500 simple function passing test for workers

--- web/src/main/resources/org/geogebra/web/resources/war/offscreenPlotting.js
+++ web/src/main/resources/org/geogebra/web/resources/war/offscreenPlotting.js
@@ -1,8 +1,44 @@
+self.window = function() {};
+importScripts('jsonfn.js');
 self.onmessage = (evt) => {
   console.log(evt.data);
   const canvas = evt.data.canvas;
-//  const view = evt.data.view;
-//  const gp = evt.data.gp;
+  const fn = JSONfn.parse(evt.data.evalFn);
+  console.log(fn(3));
+  var count = 0;
+
+function view() {};
+
+view.real2viewCoords = function(a,b) {
+	return {x: a/3, y: b/3};
+};
+
+function gp() {
+
+    this.currentX = NaN;
+    this.currentY = NaN;
+
+
+};
+
+    gp.moveTo = function(x, y) {
+        ctx.moveTo(x, y);
+        this.currentX = x;
+        this.currentY = y;
+    }
+
+    gp.lineTo = function(x, y) {
+	count++;
+	    ctx.lineTo(x, y);
+        this.currentX = x;
+        this.currentY = y;
+    }
+
+    gp.getCurrentPoint = function() {
+        return [this.currentX, this.currentY];
+    }
+
+
   const ctx = canvas.getContext("2d");
   ctx.beginPath();
   ctx.moveTo(0,0);
