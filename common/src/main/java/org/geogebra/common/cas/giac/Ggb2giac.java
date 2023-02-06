469efabc517120e00ee6f3cdf1fcec44fb6589a4
laszlogal <latz.gal@gmail.com>
2023-02-06 10:25:50 +0100

    APPS-4632 CASRegressionTest is green.

--- common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
+++ common/src/main/java/org/geogebra/common/cas/giac/Ggb2giac.java
@@ -1315,7 +1315,7 @@ public class Ggb2giac {
 							+ "["
 								+ "[odeans:=desolve(y'=%0,x,y,%1)],when(size(odeans)==0"
 									+ ",desolve(when((%0)[0]==equal,%0,y'=%0),x,y,%1)[0],"
-									+ "when(size(odeans)==1,"
+									+ "when(size(odeans) == 1,"
 										+ "normal(y=odeans[0]),"
 										+ "["
 											+ "[diff0:=evalf(subst(odeans,{x=xcoord(%1),y=ycoord(%1)}))],"
@@ -1323,8 +1323,11 @@ public class Ggb2giac {
 						// note: both could go through, pick just one
 											+ "when(abs(diff0[0]-ycoord(%1))<abs(diff0[1]-ycoord(%1)),"
 												+ "normal(odeans[0]),"
-												+ "normal(odeans[1]))"
-												+ "][-1]))][-1]"
+												+ "normal(odeans[1])"
+						+ ")"
+												+ "][-1])),"
+												+ "[xx:=[desolve(when((%0)[0]==equal,%0,y'=%0),x,y,%1)]"
+						+ "],xx[0][-1]][-1]"
 						 + "]"
 						+ ")[0]"
 		);
